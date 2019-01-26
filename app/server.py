8ef683194e55ec48f77ebab9b9f11cc2b9747a9b
Kartik Verma <vkartik97@gmail.com>
2019-01-26 20:23:41 +0530

    Added Multi Threading for External Dependency

--- app/server.py
+++ app/server.py
@@ -1,19 +1,21 @@
 from flask import Flask
-
+import time
+import threading
 
 app = Flask(__name__)
 
-# Crawler should be called here
-@app.route("/run_crawler")
+@app.route("/run")
 def index():
-    # This is where Crawler is be called
+    def handle_sub_view():
+        for i in range(0, 5):
+            print("Hello World!")
+            time.sleep(1)
+    threading.Thread(target=handle_sub_view).start()
     return "Hello World!"
-
 
 @app.route("/test")
 def test():
     return "Server is working!"
-
 
 if __name__ == '__main__':
    app.run()
